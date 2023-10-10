cb6c6234fc31f2373e05058a353f904916fd2bd9
Tapan2907 <102475283+Tapan2907@users.noreply.github.com>
2023-10-10 14:15:40 +0530

    Add files via upload

---
+++ Image_Classification.py
@@ -0,0 +1,63 @@
+
+import scipy
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+training_set = train_datagen.flow_from_directory('dataset/dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_set = test_datagen.flow_from_directory('dataset/dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+
+cnn = tf.keras.models.Sequential()
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.layers.Flatten())
+
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
+
+
+
+import numpy as np
+from keras.preprocessing import image
+
+inp = ''
+while(inp!='quit'):
+    inp=input('Enter file name (quit to close: ')
+    try:
+        test_image = image.load_img('dataset/dataset/single_prediction/'+ inp , target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = cnn.predict(test_image)
+        training_set.class_indices
+        if result[0][0] == 1:
+            prediction = 'dog'
+        else:
+            prediction = 'cat'
+
+        print(prediction)
+    except FileNotFoundError:
+        print('File not present or entered wrong file name(P.S.-Eneter file name with extension. ')
